ccc6209b50bcd42d39693b172b9be62cbcd6c2f6
Daniel Rubin <daniel.rubin1997@gmail.com>
2022-12-08 13:31:38 +0100

    added firebase

---
+++ src/app/game-info/game-info.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-game-info',
+  templateUrl: './game-info.component.html',
+  styleUrls: ['./game-info.component.scss']
+})
+export class GameInfoComponent implements OnInit, OnChanges{
+  cardAction = [
+    { title: 'Waterfall', description: 'Everyone has to start drinking at the same time. As soon as player 1 stops drinking, player 2 is allowed to stop and so on.' },
+    { title: 'You', description: 'You decide who drinks.' },
+    { title: 'Me', description: 'Congrats! Drink a shot!' },
+    { title: 'Category', description: 'Come up with a category (e.g. Colors). Each Player must enumerate one item from the category.' },
+    { title: 'Bust a jive', description: 'Player 1 makex a dace move. Player 2 repeats the dance move and adds a second one.' },
+    { title: 'Chicks', description: 'All girls drink. ' },
+    { title: 'Heaven', description: 'Put your hands up! The last player drinks!' },
+    { title: 'Mate', description: 'Pick a mate. Your mate must always drink when you drnk and the other was around.' },
+    { title: 'Thumbmaster', description: '' }, ,
+    { title: 'Men', description: 'All men drink' },
+    { title: 'Quizmaster', description: '' },
+    { title: 'Never have I ever...', description: 'Say something you never did. Everyone who did it has to drink.' },
+    { title: 'Rule', description: 'Make a rule. Everyone needs to drink when he breaks the rule.' }
+  ];
+
+  title: string = '';
+  description: string = '';
+  @Input() card: string;
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  ngOnChanges(): void {
+    if(this.card) {
+      let cardNumber = +this.card.split('_')[1];
+      this.title = this.cardAction[cardNumber - 1].title;
+      this.description = this.cardAction[cardNumber - 1].description;
+    }
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'ring-of-fire-b3369',
+    appId: '1:893051135831:web:de87473cb2e3397a338012',
+    storageBucket: 'ring-of-fire-b3369.appspot.com',
+    apiKey: 'AIzaSyDQ9idCP9UKmnok2GpeB1gaC_N1Qoi01Aw',
+    authDomain: 'ring-of-fire-b3369.firebaseapp.com',
+    messagingSenderId: '893051135831',
+  },
   production: false
 };
 
